3f647e5b917814cbbec6e79104c108546ff34022
delio404 <adelio.a.castro@gmail.com>
2023-08-30 22:46:54 -0300

    first commit

---
+++ src/main/java/com/delios/screenmach/Filme.java
@@ -0,0 +1,4 @@
+package com.delios.screenmach;
+
+public class Filme {
+}
---
+++ src/main/java/com/delios/screenmach/ScreenmachApplication.java
@@ -0,0 +1,13 @@
+package com.delios.screenmach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScreenmachApplication.class, args);
+    }
+
+}
